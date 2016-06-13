ce6f36468e40de662e31da90cc9c675f2fcf226d
Kurt <kurt.gooding@gmail.com>
2016-06-13 00:36:10 -0700

    doc

--- test/lib.js
+++
@@ -1,9 +0,0 @@
-import rewire from 'rewire'
-const should = require('chai').should()
-
-describe('lib', () => {
-  const lib = rewire('../lib').default
-
-  it('should exist', () => should.exist(lib))
-  it('should be a function', () => lib.should.be.a('function'))
-})
