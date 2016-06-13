2e041ff1b67a1c5b89aa958fb7f8c8b290f7d37e
Kurt <kurt.gooding@gmail.com>
2016-06-13 00:22:56 -0700

    unit test fix

--- test/lib.js
+++ test/lib.js
@@ -2,8 +2,8 @@ import rewire from 'rewire'
 const should = require('chai').should()
 
 describe('lib', () => {
-  const lib = rewire('../lib')
+  const lib = rewire('../lib').default
 
-  it('should exist', () => should.exist(lib.default))
+  it('should exist', () => should.exist(lib))
   it('should be a function', () => lib.should.be.a('function'))
 })
